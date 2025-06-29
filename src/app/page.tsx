3695306b6e460e2f74d4aa281333b6632849e425
TariqKichawele <tariqkichawele66@gmail.com>
2025-06-29 09:55:03 -0400

    Add Prisma setup with initial schema and database configuration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,5 @@
-import { Button } from "@/components/ui/button";
 
-  
-export default function Home() {
-  return (
-    <div>
-      <Button variant="new">Click me</Button>
-    </div>
-  );
+
+export default async function Home() {
+  return <div>Hello World</div>;
 }
