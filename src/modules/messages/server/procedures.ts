9106038140bcf29121ff49e1456c6b7cb323f148
TariqKichawele <tariqkichawele66@gmail.com>
2025-06-30 13:18:58 -0400

    Refactor database schema to remove User and Post models, introducing Message and Fragment models instead. Implement MessageRole and MessageType enums for better message categorization. Update Inngest functions and TRPC procedures to handle message creation and retrieval, enhancing the messaging system.

---
+++ src/modules/messages/server/procedures.ts
@@ -0,0 +1,38 @@
+import { inngest } from "@/inngest/client";
+import { prisma } from "@/lib/db";
+import { baseProcedure, createTRPCRouter } from "@/trpc/init";
+import z from "zod";
+
+export const messagesRouter = createTRPCRouter({
+    getMany: baseProcedure
+        .query(async () => {
+            return prisma.message.findMany({
+                orderBy: { createdAt: "desc" },
+            });
+        }),
+    create: baseProcedure
+        .input(
+            z.object({
+                value: z.string().min(1, { message: "Message cannot be empty" }),
+            })
+        )
+        .mutation(async ({ input }) => {
+            const newMessage = await prisma.message.create({
+                data: {
+                    content: input.value,
+                    role: "USER",
+                    type: "RESULT",
+                }
+            });
+
+            await inngest.send({
+                name: "code-agent/run",
+                data: {
+                    value: input.value,
+                }
+            })
+
+            return newMessage;
+           
+        })
+})
